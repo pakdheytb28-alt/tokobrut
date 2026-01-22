d1617b466260ce0761ed1f48dee948b5b8135d2c
aziez <pakdheytb28@gmail.com>
2026-01-23 00:53:21 +0700

    Fix class not found error

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// 1. PENTING: Load "Kamus" Laravel (Autoloader) dulu!
+// 1. PENTING: Load "Kamus" (Autoloader) agar Laravel dikenali
 require __DIR__ . '/../vendor/autoload.php';
 
 // 2. Load Aplikasi Laravel
@@ -8,18 +8,21 @@ $app = require __DIR__ . '/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
-| Fix Vercel Read-Only Error
+| Fix Vercel Read-Only Error (Jurus Anti-Gagal)
 |--------------------------------------------------------------------------
+| Kita paksa Laravel menggunakan folder /tmp untuk menyimpan cache & log.
+| Folder /tmp adalah satu-satunya tempat yang boleh ditulis di Vercel.
 */
+
 $storage = '/tmp/storage';
 
-// Buat folder storage sementara di /tmp jika belum ada
+// Buat folder storage sementara jika belum ada
 if (!is_dir($storage)) {
     mkdir($storage, 0777, true);
     mkdir($storage . '/framework/views', 0777, true);
 }
 
-// Paksa Laravel menggunakan storage di /tmp (karena Vercel Read-Only)
+// Perintahkan Laravel untuk memakai folder /tmp tersebut
 $app->useStoragePath($storage);
 
 // 3. Jalankan Aplikasi
